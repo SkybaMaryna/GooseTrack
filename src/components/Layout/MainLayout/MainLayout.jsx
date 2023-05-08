31340c5354cb9a7bee41831d0d722dc88ad78fef
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-05-08 20:57:55 +0200

    markup

--- src/components/Layout/MainLayout/MainLayout.jsx
+++ src/components/Layout/MainLayout/MainLayout.jsx
@@ -1,5 +1,9 @@
+import AuthNavigate from "components/shared/AuthNavigate/AuthNavigate";
+
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return <div>MainLayout
+      <AuthNavigate/>
+  </div>;
 };
 
 export default MainLayout;
--- src/components/shared/AuthNavigate/AuthNavigate.jsx
+++ src/components/shared/AuthNavigate/AuthNavigate.jsx
@@ -1,5 +1,46 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
 const AuthNavigate = () => {
-  return <div>AuthNavigate</div>;
+  return <div>
+    <StyledNavLink to="/registration">Sing Up</StyledNavLink>
+    <NavLink to="/login">
+        <Button>Log in</Button>
+      </NavLink>
+  </div>;
 };
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: underline;
+  font-family: 'Inter';
+  color: var(--mainWhite);
+  font-size: 14px;
+  line-height: 1.29;
+  font-weight: 600;
+  background-color: var(--mainBlue);
+  padding: 12px;
+  margin: 4px;
+
+   &:hover {
+    text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07), 0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);}
+  `;
+
+  export const Button = styled.button`
+  border-radius: 16px;
+  font-family: 'Inter';
+  font-size: 14px;
+  line-height: 1.29;
+  font-weight: 600;
+  margin: 4px;
+  width: 121px;
+  height:49px;
+  color: var(--mainBlue);
+  background-color: var(--mainWhite);
+  box-shadow: 4px 2px 16px rgba(136, 165, 191, 0.48);
+  cursor: pointer;
+  /* &:hover {
+    background-color: #2B78EF;
+  } */
+  `;
 
 export default AuthNavigate;
