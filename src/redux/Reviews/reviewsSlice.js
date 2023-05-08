764fbbc3b949163ad08a388f40a8f819bff860b7
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-08 20:34:51 +0300

    add store

--- src/redux/Reviews/reviewsSlice.js
+++ src/redux/Reviews/reviewsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import fetchReviews from './reviewsOperations';
+import { fetchReviews } from './reviewsOperations';
 
 const initialState = {
   reviews: [],
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,3 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { tasksReducer } from './Tasks/tasksSlice';
 import { reviewsReducer } from './Reviews/reviewsSlice';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'token',
+  version: 1,
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    // auth: persistReducer(persistConfig, authReducer),
+    tasks: tasksReducer,
+    reviews: reviewsReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+export const persistor = persistStore(store);
