c97ecc0f638557a37f693c550b49d883896e3f0a
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-05-08 22:36:46 +0200

    AuthNavigate

--- src/components/Layout/MainLayout/MainLayout.jsx
+++ src/components/Layout/MainLayout/MainLayout.jsx
@@ -1,9 +1,7 @@
 import AuthNavigate from "components/shared/AuthNavigate/AuthNavigate";
 
 const MainLayout = () => {
-  return <div>MainLayout
-      <AuthNavigate/>
-  </div>;
+  return <div>MainLayout</div>;
 };
 
 export default MainLayout;
--- src/components/shared/AuthNavigate/AuthNavigate.jsx
+++ src/components/shared/AuthNavigate/AuthNavigate.jsx
@@ -1,15 +1,22 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { FiLogIn } from "react-icons/fi";
 
 const AuthNavigate = () => {
-  return <div>
-    <StyledNavLink to="/registration">Sing Up</StyledNavLink>
+  return <LinksBox>
+    <StyledNavLink to="/registration">Sign Up</StyledNavLink>
     <NavLink to="/login">
-        <Button>Log in</Button>
+        <ButtonLogIN>Log in
+          <IconLogIn><FiLogIn/></IconLogIn>
+        </ButtonLogIN>
       </NavLink>
-  </div>;
+  </LinksBox>;
 };
-
+const LinksBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content:center; 
+`
 const StyledNavLink = styled(NavLink)`
   text-decoration: underline;
   font-family: 'Inter';
@@ -25,22 +32,33 @@ const StyledNavLink = styled(NavLink)`
     text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07), 0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);}
   `;
 
-  export const Button = styled.button`
+  export const ButtonLogIN = styled.button`
+  border-color:var(--mainBlue);
   border-radius: 16px;
   font-family: 'Inter';
   font-size: 14px;
   line-height: 1.29;
   font-weight: 600;
-  margin: 4px;
+  margin-left: 20px;
   width: 121px;
   height:49px;
   color: var(--mainBlue);
   background-color: var(--mainWhite);
-  box-shadow: 4px 2px 16px rgba(136, 165, 191, 0.48);
+  display: flex;
+  align-items: center;
+  justify-content:center;
+  /* box-shadow: 4px 2px 16px rgba(136, 165, 191, 0.48); */
   cursor: pointer;
   /* &:hover {
     background-color: #2B78EF;
   } */
   `;
+ const IconLogIn = styled.span`
+ width: 18px;
+ height:18px;
+ size: 18px;
+ padding-top:1px;
+ margin-left: 7px;
+ `
 
 export default AuthNavigate;
