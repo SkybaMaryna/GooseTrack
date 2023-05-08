049e37cffacc13a4004089f03be2ecbc8f3eacd5
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-08 20:12:05 +0300

    add NotFound and Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
 import HomePage from 'pages/HomePage/HomePage';
 import LoginPage from 'pages/LoginPage/LoginPage';
 import RegisterPage from 'pages/RegisterPage/RegisterPage';
-import { Route, Routes } from 'react-router-dom';
+import MainLayout from './Layout/MainLayout/MainLayout';
+import AccountPage from 'pages/AccountPage/AccountPage';
+import CalendarPage from 'pages/CalendarPage/CalendarPage';
+import ChoosedDay from './Calendar/ChoosedDay/ChoosedDay';
+import ChoosedMonth from './Calendar/ChoosedMonth/ChoosedMonth';
+import NotFound from './NotFound/NotFound';
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="account" element={<AccountPage />} />
+          <Route path="calendar" element={<CalendarPage />}>
+            <Route path="calendar/day/:currentDay" element={<ChoosedMonth />} />
+            <Route
+              path="calendar/month/:currentDate"
+              element={<ChoosedDay />}
+            />
+            <Route />
+          </Route>
+        </Route>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
