5e11e50a04fa10b2c7e5a4fd5a3d83d1eb944fdc
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-05-09 19:00:13 +0200

    HomePage wiyhout picture

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,76 +1,79 @@
 
 import AuthNavigate from "components/shared/AuthNavigate/AuthNavigate";
+import { HeroGooseImg, HeroTitle, Herosection, InfoBox, MainInfoBox, MainNumber, MainSubTitle, MainText, MainTitle, SpanOO } from "./HomePage.styled";
 
 const HomePage = () => {
-  return <section>
-    <div>
-    {/* <picture>
+  return <div>
+     <Herosection>
+      <div>
+   <picture>
       <source
-        media="(min-width: 1280px)"
-        srcset="../../images/heroSection/GOOSE 2_desk@1x.png, ../../images/heroSection/GOOSE 2_desk@2x.png 2x"
+        media="(min-width: 1440px)"
+        srcSet="../../images/heroSection/GOOSE_2_desk@1x.png, ../../images/heroSection/GOOSE_2_desk@2x.png 2x"
         type="image/png"
       />
-
       <source
         media="(min-width: 768px)"
-        srcset="../../images/heroSection/GOOSE 2_tab@1x.png, ../../images/heroSection/GOOSE 2_tab@2x.png 2x"
+        srcSet="../../images/heroSection/GOOSE_2_tab@1x.png, ../../images/heroSection/GOOSE_2_tab@2x.png 2x"
         type="image/png"
       />
-
       <source
         media="(max-width: 767px)"
-        srcset="../../images/heroSection/GOOSE 2_mob@1x.png, ../../images/heroSection/GOOSE 2_mob@2x.png 2x"
+        srcSet="../../images/heroSection/GOOSE_2_mob@1x.png, ../../images/heroSection/GOOSE_2_mob@2x.png 2x"
         type="image/png"
       />
       <img
-        src=" ../../images/heroSection/GOOSE 2_desk@1x.png"
+        src={require("../../images/heroSection/GOOSE_2_desk@1x.png")}
         alt="goose welcome"
         width="150"
         height="149"
       />
-    </picture> */}
-    <img
-        src=" ../../images/heroSection/GOOSE_2_desk@1x.png"
+    </picture> 
+    
+    {/* <HeroGooseImg
+        src={require("../../images/heroSection/GOOSE_2_desk@1x.png")}
         alt="goose welcome"
         width="150"
         height="149"
-      />
-    <h1>GooseTrack</h1>
+      /> */}
+       </div>
+    <HeroTitle>G<SpanOO>oo</SpanOO>seTrack</HeroTitle>
     <AuthNavigate/>
-    </div>
-    <div>
-      <h1>1.      
-      </h1>
-      <h2><div>CALENDAR</div></h2>
-      <h3>VIEW</h3>
-      <p>GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.</p>
+    </Herosection>
+    <MainInfoBox>
+      <InfoBox>
+      <MainNumber>1.      
+      </MainNumber>
+      <MainTitle>CALENDAR</MainTitle>
+      <MainSubTitle>VIEW</MainSubTitle>
+      <MainText>GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.</MainText>
+      </InfoBox>
       <picture>
       <source
-        media="(min-width: 1280px)"
-        srcset="../../images/heroSection/GOOSE 2_desk@1x.png, ../../images/heroSection/GOOSE 2_desk@2x.png 2x"
+        media="(min-width: 1440px)"
+        srcSet="../../images/mainPage/image1_desk@1x.png, ../../images/mainPage/image1_desk@2x.png 2x"
         type="image/png"
       />
-
       <source
         media="(min-width: 768px)"
-        srcset="../../images/heroSection/GOOSE 2_tab@1x.png, ../../images/heroSection/GOOSE 2_tab@2x.png 2x"
+        srcSet="../../images/mainPage/image1_tab@1x.png, ../../images/mainPage/image1_tab@2x.png 2x"
         type="image/png"
       />
 
       <source
         media="(max-width: 767px)"
-        srcset="../../images/heroSection/GOOSE 2_mob@1x.png, ../../images/heroSection/GOOSE 2_mob@2x.png 2x"
+        srcSet="../../images/mainPage/image1_mob@1x.png, ../../images/mainPage/image1_mob@2x.png 2x"
         type="image/png"
       />
       <img
-        src=" ../../images/heroSection/GOOSE 2_desk@1x.png"
+        src=" ../../images/mainPage/image1_desk@1x.png"
         alt="goose welcome"
-        width="150"
-        height="149"
+        width="604"
+        height="700"
       />
     </picture>
+    </MainInfoBox>
     </div>
-    </section>;
 };
 
 export default HomePage;
