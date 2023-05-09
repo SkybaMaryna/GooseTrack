290645d7af641fca4c09e2dfcc8b70b8aa8fe383
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-09 08:53:45 +0300

    update tailwind

--- src/components/Layout/MainLayout/MainLayout.jsx
+++ src/components/Layout/MainLayout/MainLayout.jsx
@@ -1,5 +1,21 @@
+import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
+import SideBar from '../SideBar/SideBar';
+import { StyledContainer } from './MainLayout.styled';
+
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <StyledContainer>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <SideBar />
+        <Outlet />
+      </main>
+    </StyledContainer>
+  );
 };
 
 export default MainLayout;
