2af336f4df27462034d6b1c7a3adfd3b77285789
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-09 13:11:30 +0300

    add SideBar

--- src/components/Layout/SideBar/SideBar.jsx
+++ src/components/Layout/SideBar/SideBar.jsx
@@ -1,5 +1,17 @@
+import UserNav from '../UserNav/UserNav';
+import LogoutBtn from '../LogoutBtn/LogoutBtn';
+import { useDispatch } from 'react-redux';
+import { logoutThunk } from 'redux/Auth/authOperations';
+
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <UserNav />
+      <LogoutBtn onClick={() => dispatch(logoutThunk())} />
+    </div>
+  );
 };
 
 export default SideBar;
