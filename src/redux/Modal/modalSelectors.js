5b7a99044aa723cfbdc8e9216ed17417f7c05905
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-10 14:16:32 +0300

    update modalSlice

--- src/redux/Modal/modalSelectors.js
+++ src/redux/Modal/modalSelectors.js
@@ -1,5 +1,4 @@
 export const selectAddTaskOpen = state => state.modal.isModalAddTaskOpen;
 export const selectUpDateTaskModal = state => state.modal.isModalUpDateTask;
 export const selectModalConfirmation = state => state.modal.isModalConfirmation;
-export const selectModalLogout = state => state.modal.isModalLogOut;
 export const selectSideBar = state => state.modal.isSideBar;
