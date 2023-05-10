8e14cb59c354206d40052d1533ad300d24de9d36
Maksym Lepikhov <Werzeragna@icloud.com>
2023-05-10 12:28:32 +0300

    UserNav
    User Nav component v1.0

--- src/components/Layout/UserNav/UserNav.jsx
+++ src/components/Layout/UserNav/UserNav.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { IconWrapper, LinkWrapper, NavLinkStyled, NavSecondaryTitle, NavTitle, UserNavContainer } from './UserNav.styled';
+import { BsCalendar, BsFillPersonCheckFill } from 'react-icons/bs';
+
 
 const UserNav = () => {
-  return <div>UserNav</div>;
+    return (
+        <UserNavContainer>
+        <NavTitle>User panel</NavTitle>
+            <LinkWrapper>
+                <NavLinkStyled to="/account">
+                    <IconWrapper>
+                    <BsFillPersonCheckFill/>
+                    <NavSecondaryTitle>My account</NavSecondaryTitle>
+                    </IconWrapper>
+                </NavLinkStyled>
+            </LinkWrapper>
+            <LinkWrapper>
+                <NavLinkStyled to="/calendar">
+                    <IconWrapper>
+                    <BsCalendar/>
+                    <NavSecondaryTitle>Calendar</NavSecondaryTitle>
+                    </IconWrapper>
+                </NavLinkStyled>
+            </LinkWrapper>
+        </UserNavContainer>
+    );
 };
+
+
 
 export default UserNav;
