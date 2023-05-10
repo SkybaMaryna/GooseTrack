970d8eb88a7c2034275993bc869953ec0f4e0b56
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-10 21:55:58 +0300

    update userInfo

--- src/components/Layout/UserInfo/UserInfo.jsx
+++ src/components/Layout/UserInfo/UserInfo.jsx
@@ -6,9 +6,24 @@ const UserInfo = () => {
   const userInfo = useSelector(selectUser);
   const isLoading = useSelector(selectIsIsLoading);
 
-  const name = userInfo.user?.name || 'Name';
+  const name = userInfo?.name || 'Name';
+  const avatar = userInfo?.userImgUrl;
+  const firstLetter = name.trim().slice(0, 1).toUpperCase();
 
-  return <div>UserInfo</div>;
+  return (
+    <Link to="/main/account">
+      <Name>{name}</Name>
+      <Avatar>
+        {isLoading ? (
+          <AvatarLetter>{firstLetter}</AvatarLetter>
+        ) : !avatar ? (
+          <AvatarLetter>{firstLetter}</AvatarLetter>
+        ) : (
+          <AvatarImg src={avatar} alt="Avatar" />
+        )}
+      </Avatar>
+    </Link>
+  );
 };
 
 export default UserInfo;
