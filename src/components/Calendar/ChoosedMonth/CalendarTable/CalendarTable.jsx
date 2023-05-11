aa4ab404757fa98355e489269fed05c38882f4e2
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-11 22:10:36 +0300

    update CalendarTable

--- src/components/Calendar/ChoosedMonth/CalendarTable/CalendarTable.jsx
+++ src/components/Calendar/ChoosedMonth/CalendarTable/CalendarTable.jsx
@@ -37,7 +37,7 @@ const CalendarTable = ({ startDay, today }) => {
             isSelectedMonth={isSelectedMonth(dayItem)}
             type="button"
             onClick={() => {
-              navigate(`/calendar/day/${dayItem.format('YYYY-MM-DD')}`);
+              navigate(`/main/calendar/day/${dayItem.format('YYYY-MM-DD')}`);
             }}
           >
             <RowInCell justifyContent={'flex-end'}>
