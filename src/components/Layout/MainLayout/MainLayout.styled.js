91565093cf9cd9acee2c81e347266a78651a1d12
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-14 20:28:34 +0300

    update container

--- src/components/Layout/MainLayout/MainLayout.styled.js
+++ src/components/Layout/MainLayout/MainLayout.styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
   display: flex;
+  justify-content: center;
   width: 100%;
   margin: 0 auto;
   background-color: var(mainBackgroundColor);
