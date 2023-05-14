bcd8748781f92594c2f4900010f91f107525092c
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-14 09:17:14 +0300

    dark theme PeriodTypeSelect

--- src/components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.styled.js
+++ src/components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.styled.js
@@ -1,5 +1,10 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import {
+  elementBackgroundColor,
+  elementHoverBackgroundColor,
+  elementTextHoverColor,
+} from 'redux/Theme/Theme';
 
 export const List = styled.ul`
   display: flex;
@@ -13,16 +18,16 @@ export const Item = styled.li`
   font-weight: 500;
   font-size: 14px;
   line-height: 18px;
-
   display: flex;
 `;
 export const StyledNavLink = styled(NavLink)`
-  color: #3E85F3;
+  color: var(--mainBlue);
 
-  background-color:#E3F3FF;
+  background-color: ${elementBackgroundColor};
 
   &.active {
-    background-color: #CAE8FF;
+    background-color: ${elementHoverBackgroundColor};
+    color: ${elementTextHoverColor};
   }
   &.month {
     padding: 8px 16px;
@@ -43,47 +48,3 @@ export const StyledNavLink = styled(NavLink)`
     font-size: 16px;
   }
 `;
-
-
-// import styled from 'styled-components';
-
-// export const PeriodTypeSelectWrap = styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: flex-start;
-// `;
-
-// export const PeriodTypeSelectButtonMonth = styled.button`
-//   padding: 8px 16px;
-//   border: none;
-//   border-right: 1px solid rgba(62, 133, 243, 0.2);
-//   border-radius: 8px 0px 0px 8px;
-//   font-weight: 500;
-//   font-size: 16px;
-//     line-height: 1.12;
-//   color: #3e85f3;
-//   background-color: #3E85F3;
-//   text-align: center;
-//   cursor: pointer;
-//   :hover {
-//     background-color: rgb(189 222 247);
-//   }
-
-// `;
-
-// export const PeriodTypeSelectButtonDay = styled.button`
-//   padding: 8px 26px;
-//   border: none;
-//   border-radius: 0px 8px 8px 0px;
-//   font-weight: 500;
-//   font-size: 16px;
-//     line-height: 1.12;
-//   color: #3e85f3;
-//   background-color: #3E85F3;
-//   text-align: center;
-//   cursor: pointer;
-//   :hover {
-//     background-color: rgb(189 222 247);
-//   }
-
-// `;
