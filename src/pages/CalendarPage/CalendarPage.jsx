69f57b29daea8e83cbe88ef35eb83d802dc92153
Tanya <tkravche@yahoo.com>
2023-05-14 13:28:27 +0200

    fix week starts on monday

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -2,7 +2,7 @@ import moment from 'moment';
 import { useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { fetchTasks } from 'redux/Tasks/tasksOperations';
 import { CalendarToolbar } from 'components/Calendar/CalendarToolbar/CalendarToolbar';
@@ -13,8 +13,6 @@ const CalendarPage = () => {
   const [today, setToday] = useState(moment());
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  moment.updateLocale('ua', { week: { dow: 1 } });
 
   const startDay = today.clone().startOf('month').startOf('week');
 
@@ -47,7 +45,6 @@ const CalendarPage = () => {
       />
       <Outlet context={{ startDay, today, currentDate }} />
     </div>
-  
   );
 };
 
