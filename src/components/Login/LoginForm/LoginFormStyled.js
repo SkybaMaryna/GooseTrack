7ea2cdce21c0aaa7aebc73d12a3c488c1e990ae6
KsenyaKoss <roxenn34@gmail.com>
2023-05-15 08:48:37 +0200

    ok-fix-toast message

--- src/components/Login/LoginForm/LoginFormStyled.js
+++ src/components/Login/LoginForm/LoginFormStyled.js
@@ -43,11 +43,22 @@ margin-bottom: 32px;
   }
 `
 export const StyledLabel = styled.label`
+color: ${props => (props.isError ? 'red' : 'green' || 'inherit') }
 font-weight: 600;
 font-size: 12px;
 line-height: 15px;
 margin-bottom: 8px;
 margin-top: 8px;
+
+ .active__good {
+    color: green;
+ }
+
+ .active__bad {
+  color: red;
+ }
+
+
 @media screen and (min-width: 768px) {
   font-size: 14px;
   line-height: 17px;
@@ -83,15 +94,15 @@ border: 1px solid rgba(220, 227, 229, 0.6);
 border-radius: 8px;
 padding: 14px;
 
-&:focus {
+  &:focus {
   outline: none;
-  border-color: black;
-  
-}
-&:invalid {
+  border-color: black;  
+} 
+   &:invalid {
   border-color: red;
   outline: none;
 }
+
 @media screen and (min-width: 768px) { 
   padding: 18px;
   }
